2fa3b871dd53ef423f4ea8eaf12e3f38c5aed6f7
Svensson Jeremy <svensson.jeremy94@gmail.com>
2020-05-15 17:32:21 +0200

    Removed unsafe script

--- exportWorkFolders.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo rm -rf /vagrant/tezos-workplace
-mkdir /vagrant/tezos-workplace
-sudo cp -r /home/vagrant/tezos-workplace/contracts /vagrant/tezos-workplace/contracts
-sudo cp -r /home/vagrant/tezos-workplace/migrations /vagrant/tezos-workplace/migrations
-sudo cp -r /home/vagrant/tezos-workplace/test /vagrant/tezos-workplace/test
-sudo cp /home/vagrant/tezos-workplace/truffle-config.js /vagrant/tezos-workplace/truffle-config.js
